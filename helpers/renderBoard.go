ab21fc85be2a9c82959473028e86b3cbe4e52865
YeyoM <yeyomoreno2003@hotmail.com>
2024-11-25 17:00:57 -0600

    fix: :fire: Remove debug logs

--- helpers/renderBoard.go
+++ helpers/renderBoard.go
@@ -1,23 +1,10 @@
 package helpers
 
 import (
-	"fmt"
 	"github.com/gdamore/tcell/v2"
-	"os"
 )
 
 func RenderBoard(screen tcell.Screen, board Board, reservedSpaceX, reservedSpaceY int) {
-	// Create or open debug file
-	debugFile, err := os.OpenFile("debug_board.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Printf("Error opening debug file: %v\n", err)
-		return
-	}
-	defer debugFile.Close()
-
-	// Write header for this update
-	fmt.Fprintf(debugFile, "\n--- New Board Render ---\n")
-
 	for i := 0; i < board.Height; i++ {
 		for j := 0; j < board.Width; j++ {
 			if board.Board[i][j] == true {
@@ -25,9 +12,6 @@ func RenderBoard(screen tcell.Screen, board Board, reservedSpaceX, reservedSpace
 			}
 			if board.Board[i][j] == false {
 				screen.SetContent(j, i, '⬛', nil, tcell.StyleDefault.Background(tcell.ColorDefault))
-			}
-			if i >= 1 && j >= 1 && i <= 3 && j <= 3 {
-				fmt.Fprintf(debugFile, "Coordinate (%d, %d) - State: %t\n", j, i, board.Board[i][j])
 			}
 		}
 	}
--- helpers/updateBoard.go
+++ helpers/updateBoard.go
@@ -1,21 +1,7 @@
 package helpers
 
-import (
-	"fmt"
-	"os"
-)
-
 func UpdateBoard(prevBoard Board) (newBoard Board) {
-	// Create or open debug file
-	debugFile, err := os.OpenFile("debug_neighbors.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Printf("Error opening debug file: %v\n", err)
-		return
-	}
-	defer debugFile.Close()
-
 	// Write header for this update
-	fmt.Fprintf(debugFile, "\n--- New Board Update ---\n")
 
 	newBoard.Width = prevBoard.Width
 	newBoard.Height = prevBoard.Height
@@ -26,16 +12,6 @@ func UpdateBoard(prevBoard Board) (newBoard Board) {
 	for i := 0; i < prevBoard.Height; i++ {
 		for j := 0; j < prevBoard.Width; j++ {
 			neighbours := checkNeighbours(prevBoard, j, i)
-
-			// Debug logging for coordinates between 0,0 and 4,4
-			if i <= 3 && j <= 3 && i >= 1 && j >= 1 {
-				currentState := "dead"
-				if prevBoard.Board[i][j] {
-					currentState = "alive"
-				}
-				fmt.Fprintf(debugFile, "Coordinate (%d,%d) - Current: %s - Neighbors: %d\n",
-					j, i, currentState, neighbours)
-			}
 
 			if prevBoard.Board[i][j] {
 				if neighbours < 2 {
